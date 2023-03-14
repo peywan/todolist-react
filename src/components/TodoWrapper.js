8191f5ec65c6388c4cbe53f657e18b3189f37a67
peywan <peywan1999@icloud.com>
2023-03-14 05:51:09 +0100

    full todo

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { Todo } from "./Todo";
+import { EditTodoForm } from "./EditTodoForm";
+uuidv4();
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    if (todo.trim()) {
+      // check if the todo is not empty or whitespace (empty dont add to do)
+      setTodos([
+        ...todos,
+        { id: uuidv4(), task: todo.trim(), completed: false, isEditing: false },
+      ]);
+    }
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+  return (
+    <div className='TodoWrapper'>
+      <h1>
+        Get your <span className='todosText'>todos</span> done{" "}
+        <span className='today'>today!</span>
+      </h1>
+
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            task={todo}
+            key={index}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        )
+      )}
+    </div>
+  );
+};
